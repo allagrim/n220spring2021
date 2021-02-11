9c66fbe478c3b7bb469bfe2e23e708cc7c2a4962
allagrim <77701912+allagrim@users.noreply.github.com>
2021-02-10 22:20:39 -0500

    Create snowman.js

---
+++ labs/lab1/lab112/snowman.js
@@ -0,0 +1,27 @@
+function setup(){
+    createCanvas(400,800);
+   
+  }
+  //Draw Snowman
+  function draw() {
+    background(220);
+    stroke(0,0,0);
+    fill(255,255,255);
+    circle(200,115,50);
+    circle(200, 170, 60);
+    circle(200, 235,70);
+    circle(200,120,30);
+    stroke(255,255,255);
+    fill(255,255,255);
+    ellipse(200,116.5,32,25);
+    fill(0, 0, 0);
+    circle(190,110,5);
+    circle(209,110,5);
+    rect(175,90,50,10);
+    rect(190,60,20,30);
+    circle(200,155,10);
+    circle(200,175,10);
+    stroke(255,165,0);
+    line(200,115,210,120);
+    line(200,125,210,120);
+  }
