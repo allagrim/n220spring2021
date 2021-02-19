065f6b9445aaa0db455a7db32aa4b86698e09cf5
allagrim <77701912+allagrim@users.noreply.github.com>
2021-02-18 21:31:29 -0500

    Finished lab 4!

--- labs/lab4/Snake/js/app.js
+++ labs/lab4/Snake/js/app.js
@@ -1,28 +1,32 @@
+let color = ["#ff0000","#ffa500","#ffff00","#008000","#0000ff","#4b0082","#ee82ee"];
+//Rainbow colors
+var i = 0;
 function setup(){
     createCanvas(600,600);
-    
+    background(0);
 }
 
 
-let xPos = [0,0,0,0,0,0,0,0,0,0];
-//All of the x positions of the 10 circles
-let yPos = [0,0,0,0,0,0,0,0,0,0];
-
-let s = 5
-function snek(){
-    fill(0,0,0);
-
-    let phx = mouseX
-    let phy = mouseY
-
-    xPos[0] = phx
-    yPos[0] = phy
-    
-    console.log(xPos[0]);
-
-
-    circle(xPos[0],yPos[0],s)
 
 
 
+
+function draw(){
+  noStroke();
+  
+  background(0);
+    circle(mouseX,mouseY,60); 
+
+    //make i change to make the color change
+    //when framecount = 60 i adds 1
+    if(frameCount % 15 == 0){ 
+        //Makes i change, and as such the color changes
+        i = i + 1;
+        fill(color[i]);
+        console.log(frameCount);
+        //Makes i reset to 0 based off of the length of the array colors
+        if(i == color.length-1){
+            i = 0;
+        }
+    }
 }
--- labs/lab4/drippingwater/js/app.js
+++ labs/lab4/drippingwater/js/app.js
@@ -1,6 +1,38 @@
 function setup(){
-    createCanvas(600,600);
+    createCanvas(300,300);
 }
+
+let i = 0;
+let y = [0];
+let x = [0];
+//location of water drops
+
+
+//let x = random([0],[150]);
+
+
+function draw(){
+    background(255);
+    noStroke();
+    fill("#00FFFF");
+
+    if(frameCount % 10 == 0) {
+        y.push(0);
+        x.push(random([0],[300])) 
+    }  
+
+    for(let i = 0; i < y.length; i++) {
+        circle(x[i], y[i], 30);
+        y[i] = y[i] + 5;
+    }
+    
+    
+
+    
+
+}
+
+
 
 
 
