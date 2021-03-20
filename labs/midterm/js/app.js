8f2ec65ca292118898a5fac16d4b8cc39c27f727
allagrim <77701912+allagrim@users.noreply.github.com>
2021-03-20 14:11:43 -0400

    Midterm notes

--- labs/midterm/js/app.js
+++ labs/midterm/js/app.js
@@ -1,24 +1,33 @@
 let size = 10
+//this variable is the starting point for the circle size, it will never get down past 15 while the code is running.
 let color = ["#ffffff","#ffdbfd","#ffbdfb","#ff99f9","#ff78f7","#ff57f5","#bd00b1"];
+//color is being called in function position for the color of the circle.
 var i = 0;
+
 let y = []
 let x = []
+//these two arrays allow for the snake to be drawn
 let q = 0;
+//this is used only for the for loop in the draw command (* for reference)
 function setup() {
   createCanvas(900, 900);
 }
 
 function draw() {
     background(0);
+    //this fill is set with function position
     fill(color[i]);
+
     y.push(mouseY);
     x.push(mouseX);
+
     circle(x[q],y[q],size);
+    //this function is finding where the pointer is and what size and color to assign it, (~ for reference)
     position();
+    //*
     for(let q = 0; q < y.length; q++) {
-        
         circle(x[q], y[q], size);
-        
+        //To let the circle be drawn with an array
     }
     
   
@@ -32,13 +41,14 @@ function draw() {
   
 }
 
-
+//~
 function position(){
     
     if(mouseX <=650){
         size=15;
         i=0
     }
+    //all but 2 if statements are checking to see if the pointer is in between two points, the top and bottom if statements are checking to see if it exceeds them.
     if(mouseX >=625 && mouseX <=650){
         size=20;
         i=1
@@ -91,17 +101,19 @@ function position(){
 
 
 
-
+//This function checks to see if mouse is pressed and is "supposed" to animate it down
 function mousePressed(){
     
     for(let i = 0; i < y.length; i++) {
+        //this sets all points in the y array to 900 because I ran out of time to animate it
         y[i] = 900;
     }
 
 }
-
+//This function checks to see if mouse is released and returns the snake back up to the pointer
 function mouseReleased(){
     for(let i = 0; i < y.length; i++) {
+        //this sets all points in the y array back to the pointer
         y[i] = mouseY;
     }
 }
