baf55c8cd8a70869275a10735c34b8313b0fb405
allagrim <77701912+allagrim@users.noreply.github.com>
2021-04-01 23:41:53 -0400

    lab 7 fixed but now its done

--- labs/lab7/tipcalc/js/app.js
+++ labs/lab7/tipcalc/js/app.js
@@ -3,8 +3,8 @@ let tip = 0;
 let totalbill = 0;
 function tipcalc(){
     tip = bill.value * .20;
-    totalbill = bill.value*1.10;
-    console.log("Tip:",tip,"Total:", totalbill);
+    totalbill = bill.value*1.20;
+    console.log("Tip: $",tip,"Total: $",totalbill);
 }
 
 
